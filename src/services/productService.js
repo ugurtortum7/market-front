ac794827b6c26c047ffa400a9d62b7c6f63cbd56
ugurtortum7 <ugurtortum44@gmail.com>
2025-08-19 15:41:24 +0300

    Buglar Giderildi

--- src/services/productService.js
+++ src/services/productService.js
@@ -6,8 +6,6 @@ import apiClient from './apiClient';
  * Sistemdeki tüm ürünlerin listesini getirir.
  */
 export const getProducts = () => {
-  // apiClient.get, token'ı otomatik olarak ekleyerek GET isteği atar.
-  // Dönen veri, response'un 'data' özelliğinde bulunur.
   return apiClient.get('/urunler/');
 };
 
